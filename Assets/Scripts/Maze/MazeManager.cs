dc89c3299b259139036c806b26380ad6a7767c69
Tyler Baylson <thbaylson@gmail.com>
2026-03-18 20:28:36 -0400

    Generated wall and floor meshes now combine during the final steps of generation.

--- Assets/Scripts/Maze/MazeManager.cs
+++ Assets/Scripts/Maze/MazeManager.cs
@@ -7,6 +7,7 @@ using Template.Maze.Generation;
 using ServiceLocator = Template.Services.Services;
 using UnityEngine;
 using UnityEngine.ProBuilder;
+using UnityEngine.ProBuilder.MeshOperations;
 
 namespace Template.Maze
 {
@@ -87,81 +88,22 @@ namespace Template.Maze
 
         private void BuildFloors(MazeLayout layout, Transform parent)
         {
-            var size = new Vector3(config.CellSize, config.FloorThickness, config.CellSize);
+            var size = new Vector3(
+                layout.Width * config.CellSize,
+                config.FloorThickness,
+                layout.Height * config.CellSize);
+            var center = config.MazeOrigin + new Vector3(
+                ((layout.Width - 1) * config.CellSize) * 0.5f,
+                -(config.FloorThickness * 0.5f),
+                ((layout.Height - 1) * config.CellSize) * 0.5f);
 
-            for (var x = 0; x < layout.Width; x++)
-            {
-                for (var y = 0; y < layout.Height; y++)
-                {
-                    var cell = layout.GetCell(x, y);
-                    if (!cell.HasFloor) continue;
-
-                    var coordinate = new MazeCoordinate(x, y);
-                    var center = GetCellCenter(coordinate);
-                    center.y = config.MazeOrigin.y - (config.FloorThickness * 0.5f);
-
-                    CreateCube($"Floor_{x}_{y}", size, center, config.FloorMaterial, parent);
-                }
-            }
+            CreateCube("Floor", size, center, config.FloorMaterial, parent);
         }
 
         private void BuildWalls(MazeLayout layout, Transform parent)
         {
-            var horizontalWallSize = new Vector3(
-                config.CellSize + config.WallThickness,
-                config.WallHeight,
-                config.WallThickness);
-            var verticalWallSize = new Vector3(
-                config.WallThickness,
-                config.WallHeight,
-                config.CellSize + config.WallThickness);
-
-            for (var x = 0; x < layout.Width; x++)
-            {
-                for (var y = 0; y < layout.Height; y++)
-                {
-                    var coordinate = new MazeCoordinate(x, y);
-                    var center = GetCellCenter(coordinate);
-
-                    // These offsets place each wall on the shared edge between adjacent
-                    // cells so the generated mesh lines up with the logical grid exactly.
-                    if (layout.HasWall(coordinate, MazeWallDirection.North))
-                    {
-                        var wallCenter = new Vector3(
-                            center.x,
-                            config.MazeOrigin.y + (config.WallHeight * 0.5f),
-                            center.z - (config.CellSize * 0.5f));
-                        CreateCube($"NorthWall_{x}_{y}", horizontalWallSize, wallCenter, config.WallMaterial, parent);
-                    }
-
-                    if (layout.HasWall(coordinate, MazeWallDirection.West))
-                    {
-                        var wallCenter = new Vector3(
-                            center.x - (config.CellSize * 0.5f),
-                            config.MazeOrigin.y + (config.WallHeight * 0.5f),
-                            center.z);
-                        CreateCube($"WestWall_{x}_{y}", verticalWallSize, wallCenter, config.WallMaterial, parent);
-                    }
-
-                    if (y == layout.Height - 1 && layout.HasWall(coordinate, MazeWallDirection.South))
-                    {
-                        var wallCenter = new Vector3(
-                            center.x,
-                            config.MazeOrigin.y + (config.WallHeight * 0.5f),
-                            center.z + (config.CellSize * 0.5f));
-                        CreateCube($"SouthWall_{x}_{y}", horizontalWallSize, wallCenter, config.WallMaterial, parent);
-                    }
-
-                    if (x == layout.Width - 1 && layout.HasWall(coordinate, MazeWallDirection.East))
-                    {
-                        var wallCenter = new Vector3(
-                            center.x + (config.CellSize * 0.5f),
-                            config.MazeOrigin.y + (config.WallHeight * 0.5f),
-                            center.z);
-                        CreateCube($"EastWall_{x}_{y}", verticalWallSize, wallCenter, config.WallMaterial, parent);
-                    }
-                }
-            }
+            var wallSegments = CollectWallSegments(layout);
+            CreateCombinedCubes("Walls", wallSegments, config.WallMaterial, parent);
         }
 
         private void BuildExitDoor(MazeLayout layout, Transform parent)
@@ -300,6 +242,153 @@ namespace Template.Maze
                 center.z + jitterZ);
         }
 
+        private List<BoxPrimitiveSpec> CollectWallSegments(MazeLayout layout)
+        {
+            var segments = new List<BoxPrimitiveSpec>();
+            var wallCenterHeight = config.MazeOrigin.y + (config.WallHeight * 0.5f);
+            var halfCell = config.CellSize * 0.5f;
+
+            for (var y = 0; y < layout.Height; y++)
+            {
+                var x = 0;
+                while (x < layout.Width)
+                {
+                    var coordinate = new MazeCoordinate(x, y);
+                    if (!layout.HasWall(coordinate, MazeWallDirection.North))
+                    {
+                        x++;
+                        continue;
+                    }
+
+                    var runStart = x;
+                    while (x < layout.Width && layout.HasWall(new MazeCoordinate(x, y), MazeWallDirection.North))
+                    {
+                        x++;
+                    }
+
+                    var runLength = x - runStart;
+                    var startCenter = GetCellCenter(new MazeCoordinate(runStart, y));
+                    var endCenter = GetCellCenter(new MazeCoordinate(x - 1, y));
+                    var position = new Vector3(
+                        (startCenter.x + endCenter.x) * 0.5f,
+                        wallCenterHeight,
+                        startCenter.z - halfCell);
+
+                    segments.Add(new BoxPrimitiveSpec(
+                        $"NorthWallRun_{runStart}_{y}",
+                        new Vector3((runLength * config.CellSize) + config.WallThickness, config.WallHeight, config.WallThickness),
+                        position,
+                        Quaternion.identity));
+                }
+            }
+
+            if (layout.Height > 0)
+            {
+                var boundaryY = layout.Height - 1;
+                var x = 0;
+                while (x < layout.Width)
+                {
+                    var coordinate = new MazeCoordinate(x, boundaryY);
+                    if (!layout.HasWall(coordinate, MazeWallDirection.South))
+                    {
+                        x++;
+                        continue;
+                    }
+
+                    var runStart = x;
+                    while (x < layout.Width && layout.HasWall(new MazeCoordinate(x, boundaryY), MazeWallDirection.South))
+                    {
+                        x++;
+                    }
+
+                    var runLength = x - runStart;
+                    var startCenter = GetCellCenter(new MazeCoordinate(runStart, boundaryY));
+                    var endCenter = GetCellCenter(new MazeCoordinate(x - 1, boundaryY));
+                    var position = new Vector3(
+                        (startCenter.x + endCenter.x) * 0.5f,
+                        wallCenterHeight,
+                        startCenter.z + halfCell);
+
+                    segments.Add(new BoxPrimitiveSpec(
+                        $"SouthWallRun_{runStart}_{boundaryY}",
+                        new Vector3((runLength * config.CellSize) + config.WallThickness, config.WallHeight, config.WallThickness),
+                        position,
+                        Quaternion.identity));
+                }
+            }
+
+            for (var x = 0; x < layout.Width; x++)
+            {
+                var y = 0;
+                while (y < layout.Height)
+                {
+                    var coordinate = new MazeCoordinate(x, y);
+                    if (!layout.HasWall(coordinate, MazeWallDirection.West))
+                    {
+                        y++;
+                        continue;
+                    }
+
+                    var runStart = y;
+                    while (y < layout.Height && layout.HasWall(new MazeCoordinate(x, y), MazeWallDirection.West))
+                    {
+                        y++;
+                    }
+
+                    var runLength = y - runStart;
+                    var startCenter = GetCellCenter(new MazeCoordinate(x, runStart));
+                    var endCenter = GetCellCenter(new MazeCoordinate(x, y - 1));
+                    var position = new Vector3(
+                        startCenter.x - halfCell,
+                        wallCenterHeight,
+                        (startCenter.z + endCenter.z) * 0.5f);
+
+                    segments.Add(new BoxPrimitiveSpec(
+                        $"WestWallRun_{x}_{runStart}",
+                        new Vector3(config.WallThickness, config.WallHeight, (runLength * config.CellSize) + config.WallThickness),
+                        position,
+                        Quaternion.identity));
+                }
+            }
+
+            if (layout.Width > 0)
+            {
+                var boundaryX = layout.Width - 1;
+                var y = 0;
+                while (y < layout.Height)
+                {
+                    var coordinate = new MazeCoordinate(boundaryX, y);
+                    if (!layout.HasWall(coordinate, MazeWallDirection.East))
+                    {
+                        y++;
+                        continue;
+                    }
+
+                    var runStart = y;
+                    while (y < layout.Height && layout.HasWall(new MazeCoordinate(boundaryX, y), MazeWallDirection.East))
+                    {
+                        y++;
+                    }
+
+                    var runLength = y - runStart;
+                    var startCenter = GetCellCenter(new MazeCoordinate(boundaryX, runStart));
+                    var endCenter = GetCellCenter(new MazeCoordinate(boundaryX, y - 1));
+                    var position = new Vector3(
+                        startCenter.x + halfCell,
+                        wallCenterHeight,
+                        (startCenter.z + endCenter.z) * 0.5f);
+
+                    segments.Add(new BoxPrimitiveSpec(
+                        $"EastWallRun_{boundaryX}_{runStart}",
+                        new Vector3(config.WallThickness, config.WallHeight, (runLength * config.CellSize) + config.WallThickness),
+                        position,
+                        Quaternion.identity));
+                }
+            }
+
+            return segments;
+        }
+
         private Vector3 GetCellCenter(MazeCoordinate coordinate)
         {
             return config.MazeOrigin + new Vector3(
@@ -372,17 +461,53 @@ namespace Template.Maze
             meshCollider.sharedMesh = filter.sharedMesh;
         }
 
-        private void CreateCube(
+        private void CreateCombinedCubes(
             string objectName,
-            Vector3 size,
-            Vector3 position,
+            IReadOnlyList<BoxPrimitiveSpec> primitives,
             Material material,
             Transform parent)
         {
-            var cube = ShapeGenerator.GenerateCube(PivotLocation.Center, size);
-            cube.gameObject.name = objectName;
-            cube.transform.SetParent(parent, false);
-            cube.transform.position = position;
+            if (primitives == null || primitives.Count == 0)
+            {
+                return;
+            }
+
+            var meshes = new List<ProBuilderMesh>(primitives.Count);
+            for (var index = 0; index < primitives.Count; index++)
+            {
+                meshes.Add(CreateCubeMesh(primitives[index], material));
+            }
+
+            if (meshes.Count == 1)
+            {
+                FinalizeGeneratedMesh(meshes[0], objectName, material, parent);
+                return;
+            }
+
+            var target = meshes[0];
+            var combinedMeshes = CombineMeshes.Combine(meshes, target) ?? new List<ProBuilderMesh> { target };
+            var retainedMeshes = new HashSet<ProBuilderMesh>(combinedMeshes);
+
+            foreach (var mesh in meshes)
+            {
+                if (!retainedMeshes.Contains(mesh))
+                {
+                    Destroy(mesh.gameObject);
+                }
+            }
+
+            for (var index = 0; index < combinedMeshes.Count; index++)
+            {
+                var name = combinedMeshes.Count == 1 ? objectName : $"{objectName}_{index:D2}";
+                FinalizeGeneratedMesh(combinedMeshes[index], name, material, parent);
+            }
+        }
+
+        private ProBuilderMesh CreateCubeMesh(BoxPrimitiveSpec primitive, Material material)
+        {
+            var cube = ShapeGenerator.GenerateCube(PivotLocation.Center, primitive.Size);
+            cube.gameObject.name = primitive.Name;
+            cube.transform.SetPositionAndRotation(primitive.Position, primitive.Rotation);
 
             var renderer = cube.GetComponent<MeshRenderer>();
             if (renderer != null && material != null)
@@ -392,7 +517,61 @@ namespace Template.Maze
 
             cube.ToMesh();
             cube.Refresh();
-            EnsureCollider(cube);
+            return cube;
+        }
+
+        private static void FinalizeGeneratedMesh(
+            ProBuilderMesh mesh,
+            string objectName,
+            Material material,
+            Transform parent)
+        {
+            if (mesh == null)
+            {
+                return;
+            }
+
+            mesh.gameObject.name = objectName;
+            mesh.transform.SetParent(parent, true);
+
+            var renderer = mesh.GetComponent<MeshRenderer>();
+            if (renderer != null && material != null)
+            {
+                renderer.sharedMaterial = material;
+            }
+
+            mesh.ToMesh();
+            mesh.Refresh();
+            EnsureCollider(mesh);
+        }
+
+        private void CreateCube(
+            string objectName,
+            Vector3 size,
+            Vector3 position,
+            Material material,
+            Transform parent)
+        {
+            var cube = CreateCubeMesh(
+                new BoxPrimitiveSpec(objectName, size, position, Quaternion.identity),
+                material);
+            FinalizeGeneratedMesh(cube, objectName, material, parent);
+        }
+
+        private readonly struct BoxPrimitiveSpec
+        {
+            public BoxPrimitiveSpec(string name, Vector3 size, Vector3 position, Quaternion rotation)
+            {
+                Name = name;
+                Size = size;
+                Position = position;
+                Rotation = rotation;
+            }
+
+            public string Name { get; }
+            public Vector3 Size { get; }
+            public Vector3 Position { get; }
+            public Quaternion Rotation { get; }
         }
     }
 }
